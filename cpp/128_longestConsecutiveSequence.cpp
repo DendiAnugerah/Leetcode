4c46a99724dd14ebe1bb2363b3a964c96d5ee1ce
DendiAnugerah <dendiaa@students.amikom.ac.id>
2022-08-12 23:09:45 +0700

    Longest Consecutive Sequence

---
+++ cpp/128_longestConsecutiveSequence.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+#include <unordered_set>
+using namespace std;
+
+class Solution {
+public:
+    int longestConsecutive(vector<int>& nums) {
+        unordered_set<int>s(nums.begin(), nums.end());
+        int longest = 0;
+        for(auto &n: s){
+            if(!s.count(n - 1)){
+                int length = 1;
+                while (s.count(n + length))
+                    ++length;
+                longest = max(longest, length);
+        
+            }
+        }      
+        return longest;
+    }
+};
