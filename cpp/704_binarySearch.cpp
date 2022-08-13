a2347ca771b6ce9f1a616d0f729f8a5bb34f621e
DendiAnugerah <dendiaa@students.amikom.ac.id>
2022-08-13 21:59:51 +0700

    Binary Search

---
+++ cpp/704_binarySearch.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+using namespace std;
+
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int low = 0;
+        int high = nums.size() - 1;
+
+        while (low <= high){
+            int mid = low + (high - low) / 2;
+            if (nums[mid] < target){
+                low = mid + 1;
+            } else if(nums[mid] > target){
+                high = mid -1;
+            } else{
+                return mid;
+            }
+        }
+        return -1;
+    }
+};
